d73edd380b9bb310b2e8a9d68b3b911af1cc50ae
Alex <sascha.yagello@yandex.ru>
2022-06-23 17:48:00 +0300

    Properties lib updated

--- Ross/Map/MapLayout.xaml.cs
+++ Ross/Map/MapLayout.xaml.cs
@@ -1,11 +1,22 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
 using DLLSettingsControlPointForMap;
 using DLLSettingsControlPointForMap.Model;
+using Mapsui.Styles;
+using ModelsTablesDBLib;
 using Ross.JSON;
+using UIMapRast.Models;
+using WpfControlLibrary1;
+using WpfMapControl;
+using Color = Mapsui.Styles.Color;
 using Languages = ModelsTablesDBLib.Languages;
 using LocalProperties = DLLSettingsControlPointForMap.Model.LocalProperties;
+using Pen = Mapsui.Styles.Pen;
 
 namespace Ross.Map
 {
@@ -21,6 +32,7 @@ namespace Ross.Map
             Properties.OnApplyButtonClick += Properties_OnApplyButtonClick;
             LoadSettings();
             InitHotKeys();
+            InitEvaTables();
         }
 
         public SettingsControlForMap MapProperties
@@ -55,6 +67,14 @@ namespace Ross.Map
             e.Cancel = true;
         }
 
+
+        public void InitEvaTables()
+        {
+            evaTable.items = new ObservableCollection<Tabl>()
+            {
+                new Tabl(0, "Eva")
+            };
+        }
 
         #region Hot Keys
 
@@ -114,5 +134,59 @@ namespace Ross.Map
         }
 
         #endregion
+
+        private void RastrMap_OnOnPCPosition(object sender, Location e)
+        {
+            try
+            {
+                WGSCoordinate coord = new WGSCoordinate
+                {
+                    Latitude = Math.Round(e.Latitude, 6),
+                    Longitude = Math.Round(e.Longitude, 6)
+                };
+
+                ControlPost controlPost = new ControlPost(0);
+                controlPost.Coordinate = coord;
+
+               RastrMap.UpdatePC(controlPost);
+            }
+            catch { }
+        }
+
+        private void RastrMap_OnOnPointPosition(object sender, Location e)
+        {
+            try
+            {
+                WGSCoordinate coord = new WGSCoordinate
+                {
+                    Latitude = Math.Round(e.Latitude, 6),
+                    Longitude = Math.Round(e.Longitude, 6)
+                };
+
+                DefinderJammingPoint definderJammingPoint = new DefinderJammingPoint(0);
+                definderJammingPoint.Coordinate = coord;
+                definderJammingPoint.Regime = ERegime.Jamming;
+
+
+                Pen pen = new Pen();
+                pen.Color = Color.FromArgb(50, 0, 255, 0);
+                //pen.PenStrokeCap = PenStrokeCap.Square;
+                pen.PenStyle = PenStyle.Dash;
+
+
+                Zone zone = new Zone();
+                zone.PenZone = pen;
+                zone.Radius = 20000;
+                definderJammingPoint.ZoneDefinder = zone;
+                
+
+                var collection = new ObservableCollection<DefinderJammingPoint>();
+                collection.Add(definderJammingPoint);
+
+
+                RastrMap.UpdateDFP(collection);
+            }
+            catch { }
+        }
     }
 }
